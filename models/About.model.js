1b21f2884a4d5a9bdc5c5f5f6c231948acd54fd4
karantec <karanrana3095@gmail.com>
2025-03-23 03:32:30 +0530

    commit

--- models/About.model.js
+++ models/About.model.js
@@ -4,10 +4,10 @@ const aboutUsSchema = new mongoose.Schema({
     title: { type: String, required: true },         // For "DATA DRIVEN"
     description: { type: String, required: true },   // Main body content
     image: { type: String, required: true } ,
-    linkedin:{type:String, required:true},
-    facebook:{type:String,required:true} ,
-    Instagram:{type:String,required:true},
-    twitter:{type:String,required:true}
+    linkedin:{type:String},
+    facebook:{type:String} ,
+    Instagram:{type:String},
+    twitter:{type:String}
 }, { timestamps: true });
 
 module.exports = mongoose.model('AboutUs', aboutUsSchema);
