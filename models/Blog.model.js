41177c701a9819e411616039c55c38092bbd3efa
karantec <karanrana3095@gmail.com>
2025-04-29 16:32:04 +0530

    commit

--- models/Blog.model.js
+++ models/Blog.model.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const BlogSchema = new mongoose.Schema({
   title: { type: String, required: true }, // Blog post title
   image: { type: String, required: true }, // Blog cover image URL
-  description: { type: String, required: true }, // Blog post content
-  author: { type: String }, // Blog author name
+  description: { type: String, required: true }, // Blog post content // Blog author name
   createdAt: { type: Date, default: Date.now }, // Date of publication
 });
 
